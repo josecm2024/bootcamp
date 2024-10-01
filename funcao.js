3a3099c584bb998a3abbb23589fca9aaaedbf71b
josecm2024 <Administrador@gi02tads26.integrado.int>
2024-09-30 21:57:02 -0300

    cria função para calcular some e subtração

---
+++ funcao.js
@@ -0,0 +1,13 @@
+// Crie uma função em JS que receba 2 números e Calcule
+// - Soma 
+// - subtração
+// Exiba o resultados dos cálculos na tela
+
+function contas(numero1, numero2) {
+    let soma = numero1 + numero2
+    let subtracao = numero1 - numero2
+   console.log(soma)
+   console.log(subtracao)
+}
+
+contas(50,10)
