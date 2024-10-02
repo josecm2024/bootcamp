088267ddd9289ca4670e2a5b93d8ebb149825723
josecm2024 <joseluizcm2022@Outlook.com>
2024-10-02 20:02:22 -0300

    construído um array de frutas para estudo

---
+++ frutas.js
@@ -0,0 +1,7 @@
+// crie uma lista de frutas e exiba-as
+
+const frutas = ["maçã", "manga", "morango", "melancia", "maracujá", "acerola"]
+
+console.log("A fruta na posição 1 é", frutas[1])
+console.log("A fruta na posição 4 é", frutas[4])
+console.log("Meu array tem", frutas.length, "frutas")
